69c98979d57ea2ae65f0afaf71cfee61a7d9766e
Alexis Deruelle <alexis.deruelle@gmail.com>
2023-06-20 23:29:27 +0200

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -5,30 +5,30 @@
 
 #![feature(rustc_private)]
 #![deny(rustc::internal)]
+extern crate rustc_codegen_ssa;
 extern crate rustc_driver;
-extern crate rustc_interface;
 extern crate rustc_errors;
+extern crate rustc_interface;
 extern crate rustc_lint;
+extern crate rustc_metadata;
 extern crate rustc_span;
 extern crate rustc_target;
-extern crate rustc_metadata;
-extern crate rustc_codegen_ssa;
 
+use rustc_codegen_ssa::back::metadata::DefaultMetadataLoader;
+use rustc_metadata::locator;
+use rustc_span::edition::Edition;
+use rustc_target::spec::Target;
+use rustc_target::spec::TargetTriple;
 use std::io;
 use std::path::Path;
-use rustc_span::edition::Edition;
-use rustc_metadata::locator;
-use rustc_target::spec::TargetTriple;
-use rustc_target::spec::Target;
-use rustc_codegen_ssa::back::metadata::DefaultMetadataLoader;
 
 fn main() {
     println!("Hello, world!");
     let path = std::env::args().nth(1).expect("no path given");
-//    let path = Path::new("/Volumes/Build/linux-build-llvm/rust/libmacros.so");
+    //    let path = Path::new("/Volumes/Build/linux-build-llvm/rust/libmacros.so");
     let path = Path::new(&path);
     let triple = TargetTriple::from_triple("x86_64-apple-darwin");
-    let target = Target::expect_builtin(&triple); 
+    let target = Target::expect_builtin(&triple);
     let mut stdout = io::stdout();
 
     // https://stackoverflow.com/questions/75561888/cannot-access-a-scoped-thread-local-variable-without-calling-set-first
@@ -46,4 +46,3 @@ fn main() {
     //     out: &mut dyn Write
     // ) -> IoResult<()>
 }
-
